1577051d6ebeba0bf93cebc2853241ad2bde67fa
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-09-22 20:36:26 -0600

    added a few more basic component structures

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
-import Navbar from '../Navbar'
+import Navbar from '../Navbar';
+import Footer from '../Footer';
 
 function App() {
   return(
+
+    <div>
+      <Navbar />
+
+      <Footer />
+    </div>
 
   );
 }
