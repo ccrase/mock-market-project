5bd0f48dcb17af5b9a7061b3d63acf9950bffa80
Nathan Elorreaga <nathan.elorreaga@gmail.com>
2019-10-19 10:11:00 -0600

    Styled and organized cards

--- client/src/components/NewsResults/index.jsx
+++ client/src/components/NewsResults/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MDBCol, MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn } from "mdbreact";
+import { MDBContainer, MDBCol, MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn, MDBIcon } from "mdbreact";
 
 export default function NewsResults(props) {
 
@@ -10,23 +10,32 @@ export default function NewsResults(props) {
     if (newsArticles) {
         const results = newsArticles.map((result, i) => (
 
-            <MDBCol>
-            <MDBCard style={{ width: "22rem" }}>
-                <MDBCardImage className="img-fluid" src={result['image_url']} waves />
-                <MDBCardBody>
-                    <MDBCardTitle>{result['title']}</MDBCardTitle>
-                        <MDBCardText>
-                            Put some text in here
-                        </MDBCardText>
-                    <MDBBtn href={result['news_url']}>Read Article</MDBBtn>
-                </MDBCardBody>
-            </MDBCard>
-        </MDBCol>
+            <MDBCol md="4" className="float-left">
+                <MDBCard style={{ width: "15rem", height: "400px", marginBottom: "50px" }}>
+                    <MDBCardImage style={{marginBottom:"10px", width:"100%", height:'auto'}} className="img-fluid" src={result['image_url']} waves />
+                    <MDBCardBody>
+                    <MDBCardTitle style={{ fontSize: "10px" }}>{result['date'].slice(0, 14)}</MDBCardTitle>
+                        <MDBCardTitle style={{ fontSize: "15px" }}>{result['title']}</MDBCardTitle>                       
+                                <MDBCardText style={{ fontSize: "10px" }}>
+                                    <p>{result['text'].slice(0,150)}...</p>
+                                    
+                                 
+                                    
+                                    <p>Ticker(s): {result['tickers'].map((ticker) => <span>|<span style={{fontWeight: 'bold'}}> {ticker} </span> | </span>)}</p>
+                                </MDBCardText>
+                            <MDBContainer style={{textAlign:'center'}}>
+                            <MDBBtn size="sm" gradient="aqua" href={result['news_url']}>Read Article <MDBIcon icon="glasses" className="ml-1" /></MDBBtn>
+                            </MDBContainer>
+                    </MDBCardBody>
+                </MDBCard>
+            </MDBCol>
 
         ))
         return results
     }
 
-    return <div style={{ width: "22rem" }}> no results </div>
+    return (<MDBContainer fluid>
+
+    </MDBContainer>)
 
 };
