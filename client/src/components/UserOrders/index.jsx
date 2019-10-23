633fc70b9dd33edc5f7d4466ec2958e492d58560
Calli Crase <callilogancrase@gmail.com>
2019-10-22 21:54:23 -0600

    making a symbol object to use in order table

--- client/src/components/UserOrders/index.jsx
+++ client/src/components/UserOrders/index.jsx
@@ -5,18 +5,31 @@ import { PromiseProvider } from 'mongoose';
 import axios from 'axios';
 
 const UserOrder = (props) => {
-  const [ ticker, setTicker ] = useState({})
+  const [ tickers, setTickers ] = useState({})
   // console.log(props);
-
+  //define a TICKER OBJECT to hold all data
+  const tickerArray = [];
   for(var i = 0; i < props.details.length; i++){
     //if order type = BUY 
     if(props.details[i].order_type === "buy"){
       console.log(props.details[i].ticker_name);
+      //dynamically add information to symbol object
+      let symbol = {'name': props.details[i].ticker_name};      
       //run AXIOS call
-      var symbol = props.details[i].ticker_name;
-      axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=V095HJYQ4HICG0NL`)
-      .then(res => console.log(res.data["Time Series (5min)"]))
+      axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${props.details[i].ticker_name}&interval=5min&apikey=4H5CGJD3YP7ZTU0V`)
+      .then(res => {
+        //find the most recent time
+        var time = Object.keys(res.data["Time Series (5min)"])[0];
+        //find the result with the most recent time
+        var result = res.data["Time Series (5min)"][time];
+        // console.log(result);
+        symbol['current_price'] = result;
+        // setTickers(symbol);
+        console.log(symbol);
+      })
       .catch(err => console.log(err))
+      //push symbol object into tickerArray
+      tickerArray.push(symbol);
     };
     
   };
@@ -49,7 +62,7 @@ const UserOrder = (props) => {
             <tr key={i}>
               <td>{order.ticker_name}</td>
               <td>{order.quantity}</td>
-              <td>CURRENT STOCK PRICE</td>
+              <td>{order.per_stock_amount}</td>
               <td>{order.quantity} * CURRENT STOCK PRICE</td>
               <td>current s.p. - begining day s.p.</td>
               <td>current market value - ({order.quantity * order.per_stock_amount})</td>
