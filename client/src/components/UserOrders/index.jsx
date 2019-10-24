dd6676c701381ca603a5b07d641372d01f40e4b5
Calli Crase <callilogancrase@gmail.com>
2019-10-23 19:22:04 -0600

    working async code to produce data, mathematics are not finished

--- client/src/components/UserOrders/index.jsx
+++ client/src/components/UserOrders/index.jsx
@@ -5,51 +5,57 @@ import { PromiseProvider } from 'mongoose';
 import axios from 'axios';
 
 const UserOrder = (props) => {
-  const [ tickers, setTickers ] = useState({})
+  const [ tickers, setTickers ] = useState([]);
   // console.log(props);
-  //define a TICKER OBJECT to hold all data
-  const tickerArray = [];
-  for(var i = 0; i < props.details.length; i++){
-    //if order type = BUY 
-    if(props.details[i].order_type === "buy"){
-      console.log(props.details[i].ticker_name);
-      //dynamically add information to symbol object
-      let symbol = {'name': props.details[i].ticker_name};      
-      //run AXIOS call
-      axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${props.details[i].ticker_name}&interval=5min&apikey=4H5CGJD3YP7ZTU0V`)
-      .then(res => {
-        //find the most recent time
-        var time = Object.keys(res.data["Time Series (5min)"])[0];
-        //find the result with the most recent time
-        var result = res.data["Time Series (5min)"][time];
-        // console.log(result);
-        symbol['current_price'] = result;
-        // setTickers(symbol);
-        console.log(symbol);
-      })
-      .catch(err => console.log(err))
-      //push symbol object into tickerArray
-      tickerArray.push(symbol);
+
+  const handleData= async ()=>{
+    const tickerArray = [];
+    for(var i = 0; i < props.details.length; i++){
+        //dynamically add information to symbol object
+        let symbol = {'name': props.details[i].ticker_name};  
+        //add props details to the symbol obj
+        symbol['order_type'] = props.details[i].order_type;
+        symbol['quantity'] = props.details[i].quantity;
+        symbol['per_stock_amount'] = props.details[i].per_stock_amount; 
+        symbol['total_amount_invest'] = props.details[i].total_amount_earn;
+        symbol['total_amount_earn'] = props.details[i].total_amount_invest;
+        //run AXIOS call
+        await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${props.details[i].ticker_name}&interval=5min&apikey=4H5CGJD3YP7ZTU0V`)
+        .then(res => {
+          //find the most recent time
+          var time = Object.keys(res.data["Time Series (5min)"])[0];
+          //find the result with the most recent time
+          var result = res.data["Time Series (5min)"][time];
+          // console.log(result);
+          symbol['open_price'] = parseFloat(result["1. open"], 2);
+          symbol['high_price'] = parseFloat(result["2. high"], 2);
+          symbol['low_price'] = parseFloat(result["3. low"], 2);
+          symbol['close_price'] = parseFloat(result["4. close"], 2);
+          symbol['volume'] = parseInt(result["5. volume"]);
+          // setTickers(symbol);
+          tickerArray.push(symbol);
+  
+        })
+        .catch(err => console.log(err))
+        //push symbol object into tickerArray
     };
     
+    console.log(tickerArray)
+    setTickers(tickerArray);
   };
 
-
-  //similar to componentDidMount and componentDidUpdate
   useEffect(() => {
-    console.log("INSIDE USE EFFECt")
-
-    
-    return () => {
-      // cleanup
-    };
-  }, []);
+      //define a TICKER Array to hold data obj 
+      handleData();
+  
+  },[props.details] );
 
     return (
         <MDBTable small responsive hover>
         <MDBTableHead>
           <tr>
-            <th>Symbol / Name</th>
+            <th>Symbol</th>
+            <th>Type</th>
             <th>Quantity</th>
             <th>Price</th>
             <th>Market Value</th>
@@ -58,14 +64,15 @@ const UserOrder = (props) => {
           </tr>
         </MDBTableHead>
         <MDBTableBody>
-          { props.details.length > 0  ? props.details.map((order, i) => (
+          { tickers.length > 0 ? tickers.map((order, i) => (
             <tr key={i}>
-              <td>{order.ticker_name}</td>
+              <td>{order.name}</td>
+              <td>{order.order_type}</td>
               <td>{order.quantity}</td>
-              <td>{order.per_stock_amount}</td>
-              <td>{order.quantity} * CURRENT STOCK PRICE</td>
-              <td>current s.p. - begining day s.p.</td>
-              <td>current market value - ({order.quantity * order.per_stock_amount})</td>
+              <td>${order.per_stock_amount}</td>
+              <td>{order.open_price * order.quantity}</td>
+              <td>{order.high_price} - begining s.p.</td>
+              <td></td>
             </tr>
           ))
 
