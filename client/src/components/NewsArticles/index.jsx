58b62233257cf5f37a47cb7a1ff9fd5205ffaef7
Nathan Elorreaga <nathan.elorreaga@gmail.com>
2019-10-25 17:19:45 -0600

    added more css, onclick href opens in new tab

--- client/src/components/NewsArticles/index.jsx
+++ client/src/components/NewsArticles/index.jsx
@@ -6,28 +6,25 @@ const NewsArticles =(props) => {
 
     if(props.data){
         const results = props.data.map((result, i) => (
-    
+       
             <MDBCol md="4" className="float-left">
-                <MDBCard style={{ width: "25rem", height: "550px", marginBottom: "50px" }}>
+                <MDBCard className="shadow-box-example z-depth-2" style={{ width: "27rem", height: "590px", marginBottom: "50px" }}>
                     <MDBCardImage style={{marginBottom:"10px", width:"100%", height:'auto'}} className="img-fluid" src={result['image_url']} waves />
                     <MDBCardBody>
                     <MDBCardTitle style={{ fontSize: "12px" }}>{result['date'].slice(0, 14)}</MDBCardTitle>
-                        <MDBCardTitle style={{ fontSize: "20px" }}>{result['title']}</MDBCardTitle>                       
+                        <MDBCardTitle style={{ fontSize: "20px"}}>{result['title']}</MDBCardTitle>                       
                                 <MDBCardText style={{ fontSize: "15px" }}>
-                                    <p>{result['text'].slice(0,130)}...</p>
-                                    
-                                 
-                                    
+                                    <p>{result['text'].slice(0,200)}...</p> 
                                 { result['tickers'] ? <p>Ticker(s): {result['tickers'].map((ticker) => <span>|<span style={{fontWeight: 'bold'}}> {ticker} </span> | </span>)}</p> :
                                 <div></div> }      
                                 </MDBCardText>
                             <MDBContainer style={{textAlign:'center'}}>
-                            <MDBBtn size="sm" href={result['news_url']}>Read Article <MDBIcon icon="glasses" className="ml-1" /></MDBBtn>
+                            <MDBBtn  style={{marginTop:'30px'}} size="lg" gradient="blue"  href={result['news_url']} target="_blank">Read Article <MDBIcon icon="glasses" className="ml-1" /></MDBBtn>
                             </MDBContainer>
                     </MDBCardBody>
                 </MDBCard>
-                </MDBCol> 
-            ))
+            </MDBCol>
+    ))
         return results    
     }else{
         return <div>no results found</div>
