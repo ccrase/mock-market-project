b52f60b73d011023e819ab7ddef037ff1370cda3
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-10-25 19:45:10 -0600

    fixed front title

--- client/src/components/Homepage/Hero/index.jsx
+++ client/src/components/Homepage/Hero/index.jsx
@@ -4,10 +4,10 @@ import { MDBCardTitle, MDBContainer } from "mdbreact";
 const Hero = (props) => {
     return (
     <MDBContainer fluid className="homepage-hero text-center">
-        <MDBCardTitle className="h1-responsive pt-5 font-bold">
-            Trader Trainer
-        </MDBCardTitle>
-        <p className="mx-5 mb-5">
+            <MDBCardTitle className="h1-responsive pt-3 mt-5 mb-0 font-bold homepage-title">
+                Trader Trainer
+            </MDBCardTitle>
+        <p className="mx-5 mb-5 h4-responsive">
         Safely Learn To Invest
         </p>
     </MDBContainer>
